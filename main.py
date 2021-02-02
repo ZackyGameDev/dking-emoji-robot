63de558db81d9558b6eeb8e247c01c88d90d912b
Dking08 <dastageer.siddiqui@gmail.com>
2021-02-02 16:04:34 +0530

    read the errorII

--- main.py
+++ main.py
@@ -14,8 +14,56 @@ async def on_ready():
     change_status.start()
 
 @client.event
-async def on_command_error(ctx, error):
-    pass
+async def on_command_error(self, ctx, error):
+        # if command has local error handler, return
+        if hasattr(ctx.command, 'on_error'):
+            return
+
+        # get the original exception
+        error = getattr(error, 'original', error)
+
+        if isinstance(error, commands.CommandNotFound):
+            return
+
+        if isinstance(error, commands.BotMissingPermissions):
+            missing = [perm.replace('_', ' ').replace('guild', 'server').title() for perm in error.missing_perms]
+            if len(missing) > 2:
+                fmt = '{}, and {}'.format("**, **".join(missing[:-1]), missing[-1])
+            else:
+                fmt = ' and '.join(missing)
+            _message = 'I need the **{}** permission(s) to run this command.'.format(fmt)
+            await ctx.send(_message)
+            return
+
+        if isinstance(error, commands.DisabledCommand):
+            await ctx.send('This command has been disabled.')
+            return
+
+        if isinstance(error, commands.MissingPermissions):
+            missing = [perm.replace('_', ' ').replace('guild', 'server').title() for perm in error.missing_perms]
+            if len(missing) > 2:
+                fmt = '{}, and {}'.format("**, **".join(missing[:-1]), missing[-1])
+            else:
+                fmt = ' and '.join(missing)
+            _message = 'You need the **{}** permission(s) to use this command.'.format(fmt)
+            await ctx.send(_message)
+            return
+
+        if isinstance(error, commands.UserInputError):
+            await ctx.send("Invalid input.")
+            await self.send_command_help(ctx)
+            return
+
+        if isinstance(error, commands.NoPrivateMessage):
+            try:
+                await ctx.author.send('This command cannot be used in direct messages.')
+            except discord.Forbidden:
+                pass
+            return
+
+        if isinstance(error, commands.CheckFailure):
+            await ctx.send("You do not have permission to use this command.")
+            return
 
 @tasks.loop(minutes=2)
 async def change_status():
