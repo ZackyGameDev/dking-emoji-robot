a7e906a6d67a21c39e94a994318903e9406d8637
Dking08 <dastageer.siddiqui@gmail.com>
2021-02-04 23:19:51 +0530

    optimising the code

--- commands/polls.py
+++ commands/polls.py
@@ -28,8 +28,9 @@ class PollCommands(commands.Cog):
             reactions = ['👍', '👎']
 
         description = []
+        description = Description
         for x, option in enumerate(options):
-            description = Description
+            description += '\n {} {}'.format(reactions[x], option)
 
         react_message = await ctx.send(embed=discord.Embed(
             title=question,
@@ -53,8 +54,12 @@ class PollCommands(commands.Cog):
         ))
         await commands.Bot.wait_for(self.client, 'reaction_add', 
                                        check=self.check_count_reaction(int(3), react_message))
+        reaction = ctx.get(react_message.reactions, emoji='👍')
+        if reaction.count >= 3: D = 'Vote is in favour'
+        else: D = "Vote is in against the favour"
         await ctx.send(embed=discord.Embed(
             title="Majority has voted!",
+            description=D,
             color=discord.Colour.from_hsv(random(), 1, 1),
         ).set_author(
             name=f'{ctx.me}',
