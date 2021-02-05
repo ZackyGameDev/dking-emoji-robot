92b3511967a7d68eb91505b17e2671148bda369c
Dking08 <dastageer.siddiqui@gmail.com>
2021-02-05 22:58:37 +0530

    work child

--- commands/polls.py
+++ commands/polls.py
@@ -64,9 +64,9 @@ class PollCommands(commands.Cog):
         ))
         msg = await ctx.fetch_message(react_message.id)
         Reacts = str(msg.reactions).split(',')
-        results = "Votes in favour are: **{}**".format(Reacts[0].split('True count=')[1])
-        results = results + "\nVotes in not favour are: **{}**".format(Reacts[1].split('True count=')[1])
-        if int(Reacts[0].split('True count=')[1]) > int(Reacts[1].split('True count=')[1]): winner = "**In Favour**"
+        results = "Votes in favour are: **{}**".format(Reacts[0].split('True count=')[1].replace('>',''))
+        results = results + "\nVotes in not favour are: **{}**".format(Reacts[1].split('True count=')[1].replace('>',''))
+        if int(Reacts[0].split('True count=')[1].replace('>','')) > int(Reacts[1].split('True count=')[1].replace('>','')): winner = "**In Favour**"
         else: winner = "**Not in Favour**"
         await ctx.send(embed=discord.Embed(
             description= results + '\n' + winner,
