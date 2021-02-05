a569c1a142e137076a59013ae2a8681c83a61bc5
Dking08 <dastageer.siddiqui@gmail.com>
2021-02-05 22:28:15 +0530

    billie eilish

--- commands/polls.py
+++ commands/polls.py
@@ -6,63 +6,69 @@ from discord.ext import commands
 from discord.utils import get
 
 class PollCommands(commands.Cog):
-    def __init__(self, client: commands.Bot, bot):
+    def __init__(self, client: commands.Bot):
         self.client: commands.Bot = client
-        self.bot = bot
         self.client.remove_command("help")
 
-    @commands.command(pass_context=True)
-    async def quickpoll(self, ctx, question, *options: str):
-        if len(options) <= 1:
-            await self.bot.say('You need more than one option to make a poll!')
-            return
-        if len(options) > 10:
-            await self.bot.say('You cannot make a poll for more than 10 things!')
-            return
+    def check_count_reaction(self, desired_count, message):
+        def predicate(reaction, user):
+            return reaction.message == message and reaction.count>=desired_count
+        return predicate   
 
-        if len(options) == 2 and options[0] == 'yes' and options[1] == 'no':
-            reactions = ['✅', '❌']
+    @commands.command()
+    async def poll(self, ctx, question, Description='', *options: str):
+        if len(options) <= 1:
+            await ctx.send("```Error! A poll must have more than one option.```")
+            return
+        if len(options) > 2:
+            await ctx.send("```Error! Poll can have no more than two options.```")
+            return
+        if len(options) == 2 and options[0] == "yes" and options[1] == "no":
+            reactions = ['👍', '👎']
         else:
-            reactions = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣', '🔟']
+            reactions = ['👍', '👎']
 
         description = []
+        description = Description
         for x, option in enumerate(options):
             description += '\n {} {}'.format(reactions[x], option)
-        embed = discord.Embed(title=question, description=''.join(description))
-        react_message = await self.bot.say(embed=embed)
+
+        react_message = await ctx.send(embed=discord.Embed(
+            title=question,
+            color=discord.Colour.from_hsv(random(), 1, 1),
+            description = ''.join(description)
+        ).set_author(
+            name=f'{ctx.author}',
+            icon_url=f'https://cdn.discordapp.com/avatars/{ctx.author.id}/{ctx.author.avatar}.png'
+        ))
+
         for reaction in reactions[:len(options)]:
-            await self.bot.add_reaction(react_message, reaction)
-        embed.set_footer(text='Poll ID: {}'.format(react_message.id))
-        await self.bot.edit_message(react_message, embed=embed)
+            await react_message.add_reaction(reaction)
 
-    @commands.command(pass_context=True)
-    async def tally(self, ctx, id):
-        poll_message = await self.bot.get_message(ctx.message.channel, id)
-        if not poll_message.embeds:
-            return
-        embed = poll_message.embeds[0]
-        if poll_message.author != ctx.message.server.me:
-            return
-        if not embed['footer']['text'].startswith('Poll ID:'):
-            return
-        unformatted_options = [x.strip() for x in embed['description'].split('\n')]
-        opt_dict = {x[:2]: x[3:] for x in unformatted_options} if unformatted_options[0][0] == '1' \
-            else {x[:1]: x[2:] for x in unformatted_options}
-        # check if we're using numbers for the poll, or x/checkmark, parse accordingly
-        voters = [ctx.message.server.me.id]  # add the bot's ID to the list of voters to exclude it's votes
-
-        tally = {x: 0 for x in opt_dict.keys()}
-        for reaction in poll_message.reactions:
-            if reaction.emoji in opt_dict.keys():
-                reactors = await self.bot.get_reaction_users(reaction)
-                for reactor in reactors:
-                    if reactor.id not in voters:
-                        tally[reaction.emoji] += 1
-                        voters.append(reactor.id)
-
-        output = 'Results of the poll for "{}":\n'.format(embed['title']) + \
-                 '\n'.join(['{}: {}'.format(opt_dict[key], tally[key]) for key in tally.keys()])
-        await self.bot.say(output)
+        await react_message.edit(embed=discord.Embed(
+            title=question,
+            color=discord.Colour.from_hsv(random(), 1, 1),
+            description = ''.join(description)
+        ).set_author(
+            name=f'{ctx.author}',
+            icon_url=f'https://cdn.discordapp.com/avatars/{ctx.author.id}/{ctx.author.avatar}.png'
+        ))
+        await commands.Bot.wait_for(self.client, 'reaction_add', 
+                                       check=self.check_count_reaction(int(1), react_message))
+        await ctx.send(embed=discord.Embed(
+            title="Majority has voted!",
+            color=discord.Colour.from_hsv(random(), 1, 1),
+        ).set_author(
+            name=f'{ctx.me}',
+            icon_url=f'https://cdn.discordapp.com/avatars/{ctx.me.id}/{ctx.me.avatar}.png'
+        ))
+        await ctx.send(embed=discord.Embed(
+            description=(ctx.react_message.reactions),
+            color=discord.Colour.from_hsv(random(), 1, 1),
+        ).set_author(
+            name=f'{ctx.me}',
+            icon_url=f'https://cdn.discordapp.com/avatars/{ctx.me.id}/{ctx.me.avatar}.png'
+        ))
 
 def setup(client):
     client.add_cog(PollCommands(client))
