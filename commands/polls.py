b07bd047c993c91c379f25813dc220ed026b27db
Dking08 <dastageer.siddiqui@gmail.com>
2021-02-05 22:10:50 +0530

    poll system making better

--- commands/polls.py
+++ commands/polls.py
@@ -62,13 +62,35 @@ class PollCommands(commands.Cog):
             name=f'{ctx.me}',
             icon_url=f'https://cdn.discordapp.com/avatars/{ctx.me.id}/{ctx.me.avatar}.png'
         ))
-        await ctx.send(embed=discord.Embed(
-            description=react_message.reactions,
-            color=discord.Colour.from_hsv(random(), 1, 1),
-        ).set_author(
-            name=f'{ctx.me}',
-            icon_url=f'https://cdn.discordapp.com/avatars/{ctx.me.id}/{ctx.me.avatar}.png'
-        ))
+        await tally(self, ctx, react_message.id)
+
+async def tally(self, ctx, id):
+        poll_message = await self.bot.get_message(ctx.message.channel, id)
+        if not poll_message.embeds:
+            return
+        embed = poll_message.embeds[0]
+        if poll_message.author != ctx.message.server.me:
+            return
+        if not embed['footer']['text'].startswith('Poll ID:'):
+            return
+        unformatted_options = [x.strip() for x in embed['description'].split('\n')]
+        opt_dict = {x[:2]: x[3:] for x in unformatted_options} if unformatted_options[0][0] == '1' \
+            else {x[:1]: x[2:] for x in unformatted_options}
+        # check if we're using numbers for the poll, or x/checkmark, parse accordingly
+        voters = [ctx.message.server.me.id]  # add the bot's ID to the list of voters to exclude it's votes
+
+        tally = {x: 0 for x in opt_dict.keys()}
+        for reaction in poll_message.reactions:
+            if reaction.emoji in opt_dict.keys():
+                reactors = await self.bot.get_reaction_users(reaction)
+                for reactor in reactors:
+                    if reactor.id not in voters:
+                        tally[reaction.emoji] += 1
+                        voters.append(reactor.id)
+
+        output = 'Results of the poll for "{}":\n'.format(embed['title']) + \
+                 '\n'.join(['{}: {}'.format(opt_dict[key], tally[key]) for key in tally.keys()])
+        await self.bot.say(output)
 
 def setup(client):
     client.add_cog(PollCommands(client))
